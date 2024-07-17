e110a96e8dcb85eb5c61992ad41ea282dcf632de
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-07-17 11:00:11 +0530

    adjustments

--- project-6/app.js
+++ project-6/app.js
@@ -5,12 +5,6 @@ const POPULAR = "popular";
 const API_KEY = "376a7b56bf7b28c1457c230c2ebbd63a";
 const showCards = document.querySelector(".showCards");
 const movieCards = document.querySelector(".movieCards");
-const cardImg = document.querySelector('#cardImg');
-const cards = document.querySelector('.cards')
-const about = document.querySelector('.about')
-const bio = document.querySelector('.bio');
-
-
 
 const popularShows = async () => {
   try {
@@ -18,11 +12,11 @@ const popularShows = async () => {
       `${BASE_URL}/${TV}/${POPULAR}?api_key=${API_KEY}`
     );
     const data = await response.json();
-    console.log('Popular Shows Data:', data); // Debugging log
+    console.log("Popular Shows Data:", data); // Debugging log
     const showList = data.results
       .map(
         (item) =>
-          `<div class="cards relative border border-amber-400 w-[220px] h-[350px] cursor-pointer">
+          `<div class="cards relative border border-amber-400 w-[220px] h-[350px] cursor-pointer" data-show-name="${item.original_name}">
           <img
           id="cardImg"
             src="https://image.tmdb.org/t/p/w500/${item.poster_path}"
@@ -33,16 +27,22 @@ const popularShows = async () => {
           <div class="ratings absolute top-1 right-2 flex items-center gap-2 bg-black px-2 py-1 rounded-xl">
           <i class="fa-solid fa-star text-amber-400"></i>
           <div>
-            <p class="text-xs font-semibold text-white" >${(item.vote_average).toFixed(1)}/10</p>
+            <p class="text-xs font-semibold text-white" >${item.vote_average.toFixed(
+              1
+            )}/10</p>
           </div>
           </div>
           
           <div class="bio absolute px-2 w-full bottom-0">
             <div class="flex justify-between items-center mb-2">
-              <p class="text-white bg-gray-500 inline rounded-lg px-2 py-1 text-xs">${item.original_language}</p>
+              <p class="text-white bg-gray-500 inline rounded-lg px-2 py-1 text-xs">${
+                item.original_language
+              }</p>
               <p class="text-white text-xs">${item.first_air_date}</p>
             </div>
-            <p class="text-white text-left font-bold text-amber-400">${item.original_name}</p>
+            <p class="text-white text-left font-bold text-amber-400">${
+              item.original_name
+            }</p>
           </div>
         </div>`
       )
@@ -50,9 +50,16 @@ const popularShows = async () => {
 
     showCards.innerHTML = showList;
 
+    showCards.addEventListener('click', (event) => {
+      const card = event.target.closest('.cards')
+      if(card){
+        const shoeName = card.getAttribute('data-show-name')
+        alert(shoeName)
+      }
+    });
 
   } catch (error) {
-    console.error('Error fetching popular shows:', error);
+    console.error("Error fetching popular shows:", error);
   }
 };
 
@@ -62,31 +69,54 @@ const popularMovies = async () => {
       `${BASE_URL}/${MOVIE}/${POPULAR}?api_key=${API_KEY}`
     );
     const data = await response.json();
-    console.log('Popular Movies Data:', data); // Debugging log
+    console.log("Popular Movies Data:", data); // Debugging log
     const movieList = data.results
       .map(
         (item) =>
-          `<div class="cards relative border w-[220px] h-[350px] cursor-pointer">
+          `<div class="cards relative border border-amber-400 w-[220px] h-[350px] cursor-pointer" data-movie-name="${item.original_title}">
           <img
           id="cardImg"
             src="https://image.tmdb.org/t/p/w500/${item.poster_path}"
-            alt="${item.original_name}"
+            alt="${item.original_title}"
             class="w-full h-[80%] object-cover"
           />
+
+          <div class="ratings absolute top-1 right-2 flex items-center gap-2 bg-black px-2 py-1 rounded-xl">
+          <i class="fa-solid fa-star text-amber-400"></i>
+          <div>
+            <p class="text-xs font-semibold text-white" >${item.vote_average.toFixed(
+              1
+            )}/10</p>
+          </div>
+          </div>
+          
           <div class="bio absolute px-2 w-full bottom-0">
             <div class="flex justify-between items-center mb-2">
-              <p class="text-white bg-gray-500 inline rounded-lg px-2 py-1 text-xs">${item.original_language}</p>
+              <p class="text-white bg-gray-500 inline rounded-lg px-2 py-1 text-xs">${
+                item.original_language
+              }</p>
               <p class="text-white text-xs">${item.release_date}</p>
             </div>
-            <p class="text-white text-left">${item.original_title}</p>
+            <p class="text-white text-left font-bold text-amber-400">${
+              item.original_title
+            }</p>
           </div>
         </div>`
       )
       .join("");
 
     movieCards.innerHTML = movieList;
+
+    movieCards.addEventListener('click' , (event)=>{
+      const card = event.target.closest('.cards');
+      if(card){
+        const movies = card.getAttribute("data-movie-name");
+        alert(movies)
+      }
+    })
+
   } catch (error) {
-    console.error('Error fetching popular movies:', error);
+    console.error("Error fetching popular movies:", error);
   }
 };
 
